5491d608ec23fd8d58166b840400935b7432674d
hanjala <shihab2305341402@diu.edu.bd>
2025-07-04 21:19:02 +0600

    dataset practice

--- Series-&-Dataframe/series-dataframe.py
+++ Series-&-Dataframe/series-dataframe.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv('Series-&-Dataframe/winemag-data_first150k.csv')
 
 print(df.shape)   #returns the number of rows(150930) and columns(11)
+print()
+#
+print(df.info())
+print()
+#
+
