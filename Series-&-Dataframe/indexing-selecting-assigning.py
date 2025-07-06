494bbe13f0151e2b3ea97cc4dc7980a161b43ca1
hanjala <shihab2305341402@diu.edu.bd>
2025-07-06 21:14:52 +0600

    practice with datasets

--- Series-&-Dataframe/indexing-selecting-assigning.py
+++ Series-&-Dataframe/indexing-selecting-assigning.py
@@ -3,8 +3,10 @@ import pandas as pd
 reviews = pd.read_csv('Series-&-Dataframe/winemag-data_first150k.csv')
 
 # Native accessors:
-print(reviews.country)
-print()
+print(reviews.country)  #just selecting column names
+print(reviews.price)
 
 
-print(reviews.country[0])
+print(reviews.country[0]) #specifically finding a single value of column
+print(reviews.region_1[5])
+
