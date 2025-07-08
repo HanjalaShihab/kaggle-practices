00d23d322177c95ebcee7ff5416d71a07311ad60
hanjala <shihab2305341402@diu.edu.bd>
2025-07-08 21:05:18 +0600

    manipulate index

--- Series-&-Dataframe/manipulate index/manipulate.py
+++ Series-&-Dataframe/manipulate index/manipulate.py
@@ -4,3 +4,9 @@ reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.
 
 print(reviews.head())
 print(reviews.shape)
+
+
+#we can set the title column as the index column:
+x = reviews.set_index("title")
+
+print(x)
