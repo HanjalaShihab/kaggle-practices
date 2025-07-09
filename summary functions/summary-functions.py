af173a96ec4baebd57768f34b92bc7ca2a2eb9cf
hanjala <shihab2305341402@diu.edu.bd>
2025-07-09 22:51:06 +0600

    summary functions

--- summary functions/summary-functions.py
+++ summary functions/summary-functions.py
@@ -29,6 +29,3 @@ mean         88.447138
              ...      
 75%          91.000000
 max         100.000000
-Name: points, Length: 8, dtype: float64
-This method generates a high-level summary of the attributes of the given column. It is type-aware, meaning that its output changes based on the data type of the input. The output above only makes sense for numerical data; for string data here's what we get:
-
