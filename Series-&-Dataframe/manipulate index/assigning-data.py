47ebb98e996570be80c21fc62e8cb060418c11ab
hanjala <shihab2305341402@diu.edu.bd>
2025-07-09 21:05:55 +0600

    assigning data

--- Series-&-Dataframe/manipulate index/assigning-data.py
+++ Series-&-Dataframe/manipulate index/assigning-data.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.csv')
 
-reviews['critic'] = "everyone"
+reviews['critic'] = "everyone"  #adding a new column with value "everyone" to the dataset
 print(reviews.head())
 
 print(reviews.critic)
+
+print(reviews.shape)
