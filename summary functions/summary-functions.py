127809cc4c3be5197a2eaae8f5ce993855132086
hanjala <shihab2305341402@diu.edu.bd>
2025-07-09 22:46:44 +0600

    summary functions

--- summary functions/summary-functions.py
+++ summary functions/summary-functions.py
@@ -1,8 +1,13 @@
 #Pandas provides many simple "summary functions" (not an official name)
 #     which restructure the data in some useful way.
-#  For example, consider the describe() method:
 
 import pandas as pd
 
 reviews = pd.read_csv('summary functions/winemag-data-130k-v2.csv')
 
+# describe method():
+
+print(reviews.points.describe())
+print()
+print(reviews.price.describe())
+print()
