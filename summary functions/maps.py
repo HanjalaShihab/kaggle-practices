8096f5632b14a4eacbeeb18c93a9f19cc42855b1
hanjala <shihab2305341402@diu.edu.bd>
2025-07-12 22:36:47 +0600

    maps

---
+++ summary functions/maps.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+reviews = pd.read_csv('summary functions/winemag-data-130k-v2.csv')
+
+#A map is a term, borrowed from mathematics, for a function that takes 
+ #one set of values and "maps" them to another set of values.
