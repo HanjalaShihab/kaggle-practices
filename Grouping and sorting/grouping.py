6e3ac7da71f6637d347884bdaf8ef6af79d5498a
hanjala <shihab2305341402@diu.edu.bd>
2025-07-13 20:34:16 +0600

    grouping

--- Grouping and sorting/grouping.py
+++ Grouping and sorting/grouping.py
@@ -1,1 +1,5 @@
 # often we want to group our data, and then do something specific to the group the data is in.
+
+import pandas as pd
+
+reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.csv')
