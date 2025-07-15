0bda06e5428978136939a6bab301c1b186065942
hanjala <shihab2305341402@diu.edu.bd>
2025-07-15 23:56:44 +0600

    missing data

--- Data types and missing values/missing-data.py
+++ Data types and missing values/missing-data.py
@@ -4,6 +4,17 @@ reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.
 
 x = reviews[pd.isnull(reviews.country)]
 print(x)
+print("-------------------")
 
+#same as:
+y = reviews.loc[reviews.country.isnull()]
+print(y)
+
+#not necesary:
 sorted = reviews.sort_index(ascending= True)
 print(sorted)
+print("-------------------")
+
+
+#Replacing missing values:
+z = reviews.region_2.fillna("Unknown")
