b7aa33bf28385fb0bacf0c4b6b3427e50829859f
hanjala <shihab2305341402@diu.edu.bd>
2025-07-15 21:25:45 +0600

    sorting

--- Grouping and sorting/sorting.py
+++ Grouping and sorting/sorting.py
@@ -22,3 +22,25 @@ countries_reviewed = countries_reviewed.reset_index()
 
 sorted = countries_reviewed.sort_values(by= 'points')
 print(sorted)
+print("----------------------------")
+
+
+
+#Sort by Row Count Per Group:
+countries_reviewed = reviews.groupby(['country', 'province']).size().reset_index(name= 'len')
+sorted = countries_reviewed.sort_values(by= 'len', ascending= False)
+print(sorted)
+print("----------------------------")
+
+
+# To sort by index values:
+sorted = countries_reviewed.sort_index()
+#print(sorted.to_string())  to see the full dataframe
+print(sorted)
+print("----------------------------")
+
+
+#We can also sort by more than one column at a time:
+y = countries_reviewed.sort_values(by= ['country', 'len'], ascending= True)  #ascending among the groups of lens for countries not whole len
+print(y)
+print("----------------------------")
