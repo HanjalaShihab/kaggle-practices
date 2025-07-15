ca2109360a1a6a27923b7e669a985980be818f29
hanjala <shihab2305341402@diu.edu.bd>
2025-07-15 21:14:27 +0600

    sorting

--- Grouping and sorting/sorting.py
+++ Grouping and sorting/sorting.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.csv')
 
+print(reviews.columns)
+print("----------------------------")
+
+
 countries_reviewed = reviews.groupby(['country', 'province'])['points'].agg(max)
 print(countries_reviewed)
 print("----------------------------")
@@ -9,3 +13,12 @@ print("----------------------------")
 
 x = countries_reviewed.index
 print(type(x))
+print("----------------------------")
+
+
+          #sorting starting here:
+# first we have to normalize or Make the dataset  RangeIndex again:
+countries_reviewed = countries_reviewed.reset_index()
+
+sorted = countries_reviewed.sort_values(by= 'points')
+print(sorted)
