5d3778c001f552f11cc6b362bf7955f36a5bd0ea
hanjala <shihab2305341402@diu.edu.bd>
2025-07-17 16:36:57 +0600

    renaming

--- Renaming and combining/renaming.py
+++ Renaming and combining/renaming.py
@@ -3,11 +3,31 @@ import pandas as pd
 # rename() function(which let us change index names and/or column names):
 reviews = pd.read_csv('Series-&-Dataframe/manipulate index/winemag-data-130k-v2.csv')
 
-x = reviews.rename(columns= {'points': 'score'})
+reviews = reviews.rename(columns= {'points': 'score'})
+print("--------------------------")
 print(reviews.columns)
-print(x.columns)  #this will show the changed column points to score
+print("--------------------------")
 
 
 #renaming another column:
-y = reviews.rename(columns= {'region_1': 'first_region', 'region_2': 'second_region'})
-print(y.columns)
+reviews = reviews.rename(columns= {'region_1': 'first_region', 'region_2': 'second_region'})
+print(reviews.columns)
+print("--------------------------")
+
+
+#renaming index:
+z = reviews.rename(index= {
+    0: 'firstEntry',
+    1 : 'secondEntry'
+})
+print(z)   #renaming index values are very rare.For that set_index() function is usually more convenient.
+print("--------------------------")
+
+
+reviews = reviews.set_index("title")  #setting the title column as the index column
+print(reviews)
+print("--------------------------")
+
+
+x = reviews.rename_axis("wines", axis='rows').rename_axis("fields", axis='columns')
+print(x)
